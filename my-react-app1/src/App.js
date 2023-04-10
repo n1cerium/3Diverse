ddfcd432faf493e4d2808f48d3fa4871fbb4fad2
nsarubbi <nicholas.sarubbi.438@my.csun.edu>
2023-04-10 15:40:23 -0700

    react with code
    created a website skeleton

--- my-react-app1/src/App.js
+++ my-react-app1/src/App.js
@@ -2,11 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-          <h1>Hello World!</h1>
+    return (
+      <div className ="body">
+            <div className="header">
+                <p>Name</p>
+            </div>
+            <div className="navbar">
+                <a href="#" class="right">Login</a>
+                <a href="#" class="right">Cart</a>
+            </div>
+            <div className="row">
+                <div className="sidebar">
+                    <h2>learn more</h2>
+                    <h2>Reviews</h2>
+                </div>
+            <div className="main">
+               <h2>this is an item</h2>
+               <p>blaa.... </p>
+            </div>
+          </div>
       </div>
-  );
+)
 }
 
 export default App;
