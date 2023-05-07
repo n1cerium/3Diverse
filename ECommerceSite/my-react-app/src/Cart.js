abfa0911c2e065535385e1e238e585c44ce834a2
Javier Carranza <57335448+JavierACarranza@users.noreply.github.com>
2023-05-07 11:21:17 -0700

    Update Cart.js

--- ECommerceSite/my-react-app/src/Cart.js
+++ ECommerceSite/my-react-app/src/Cart.js
@@ -1,23 +1,23 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-import './Cart.css';
+    import * as React from 'react';
+    import { Link } from 'react-router-dom';
+    import './Cart.css';
 
-export default function Cart({ cartItems }) {
-  return (
-    <div className="cart-container">
-      <h1 className="cart-title">Shopping Cart</h1>
-      {(!cartItems || cartItems.length === 0) ? (
-        <p className="empty-cart-message">Your cart is empty</p>
-      ) : (
-        <ul className="cart-items-list">
-          {cartItems.map(item => (
-            <li className="cart-item" key={item.id}>
-              <span className="cart-item-name">{item.name}</span> - <span className="cart-item-price">{item.price}</span>
-            </li>
-          ))}
-        </ul>
-      )}
-      <Link to="/" className="continue-shopping-link">Continue Shopping</Link>
-    </div>
-  );
-}
+    export default function Cart({ cartItems }) {
+    return (
+        <div className="cart-container">
+        <h1 className="cart-title">Shopping Cart</h1>
+        {(!cartItems || cartItems.length === 0) ? (
+            <p className="empty-cart-message">Your cart is empty</p>
+        ) : (
+            <ul className="cart-items-list">
+            {cartItems.map(item => (
+                <li className="cart-item" key={item.id}>
+                <span className="cart-item-name">{item.name}</span> - <span className="cart-item-price">{item.price}</span>
+                </li>
+            ))}
+            </ul>
+        )}
+        <Link to="/" className="continue-shopping-link">Continue Shopping</Link>
+        </div>
+    );
+    }
