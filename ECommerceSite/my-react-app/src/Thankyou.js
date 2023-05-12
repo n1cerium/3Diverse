039010dc3209c928c77dc4a6834c9304e1c5d868
Joshua Anthony Domantay <dom.josh2701@gmail.com>
2023-05-11 23:14:07 -0700

    Add header and footer to Thank You page

--- ECommerceSite/my-react-app/src/Thankyou.js
+++ ECommerceSite/my-react-app/src/Thankyou.js
@@ -4,25 +4,46 @@ import './Thankyou.css';
 
 export default function Thankyou() {
     return (
-             
-        <div id="content">
-            <div>
-                <h1 className="frontcolor">3Diverse</h1>
-                <div class="text-row row">
-                    <div class="quote-container col-xs-10 col-xs-offset-1">
-                        <div class="tyText-container" id="quote-box">
-                           <p class="message">Thank you for shoppping with us!</p>
+        /* Joshua Header Start */
+        <div className="thankYouBody">
+            <div className="thankYouHeader">
+                <div className="thankYouLogo">
+                    <Link to="/"><h1>3Diverse</h1></Link>
+                </div>
+            </div>
+            {/* Joshua Header End */}
+
+            <div id="tyContent">
+                <div>
+                    <div class="tyText-row row">
+                        <div class="tyQuote-container col-xs-10 col-xs-offset-1">
+                            <div class="tyText-container" id="tyQuote-box">
+                            <p class="message">Thank you for shopping with us!</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <Link to="/" className="button-return-store link-no-underline" role="button">
-                    Return to Home Page
-                </Link>
+                    <div className="button-return-store-div">
+                        <Link to="/" className="button-return-store link-no-underline" role="button">
+                            Return to Home Page
+                        </Link>
+                    </div>
 
-                <Link to="/" className="button-logout link-no-underline" role="button">
-                    Log out
-                </Link>
+                    <Link to="/" className="button-logout link-no-underline" role="button">
+                        Log out
+                    </Link>
+                </div>
+            </div>
+
+            {/* Joshua Footer End */}
+            <div className="footer">
+                <div className="logo">
+                    <Link to="/"><h1>3Diverse</h1></Link>
+                </div>
+                <p>Contact us at: <a href="#" target="_blank">support@3Diverse.com</a></p>
+                <p><a href="#" target="_blank">Subscribe to our newsletter</a></p>
+                <p className="copyright">© 2023 3Diverse </p>
             </div>
         </div>
+        /* Joshua Footer Start */
     );
 } 
