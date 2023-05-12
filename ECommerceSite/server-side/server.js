a73eb4c6cb15751f9f1e54e6670cfc92e88eb205
Gevin Sarmiento <114702605+n1cerium@users.noreply.github.com>
2023-05-11 23:02:14 -0700

    Update server.js

--- ECommerceSite/server-side/server.js
+++ ECommerceSite/server-side/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const path = require("path")
 
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(express.static(path.join(__dirname + "/public")))
 
 // const db = mysql.createConnection( {
 //     host: "localhost",
@@ -114,6 +116,6 @@ app.post('/CartRemove', (req, res) => {
         }
     })
 })
-app.listen(8080, ()=>{
+app.listen(process.env.PORT || 8080, ()=>{
     console.log("listening");
 })
